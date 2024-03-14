c6ab9bed32ff6154e7619e83dfc83ba190da4d34
zhoujiandong <fire1220@live.cn>
2024-03-14 19:46:03 +0800

    MarshalJSON

--- marshal/marshaljson.go
+++ marshal/marshaljson.go
@@ -6,14 +6,30 @@ import (
 	"time"
 )
 
-type DateTime time.Time
+type DateTime struct {
+	T   time.Time
+	Tag reflect.StructTag `json:"-"`
+}
 
 func (d DateTime) MarshalJSON() ([]byte, error) {
-	t := time.Time(d)
-	if t.IsZero() {
-		return []byte(`"0000-00-00 00:00:00"`), nil
+	t := d.T
+	format := d.Tag.Get("datetime")
+	if format == "" {
+		format = time.DateTime
 	}
-	return []byte(`"` + t.Format(`2006-01-02 15:04:05`) + `"`), nil
+	mapTime := map[string]string{
+		time.DateTime: "0000-00-00 00:00:00",
+		time.DateOnly: "0000-00-00",
+		time.TimeOnly: "00:00:00",
+	}
+	if t.IsZero() {
+		if v, ok := mapTime[format]; ok {
+			return []byte(`"` + v + `"`), nil
+		} else {
+			return []byte(`""`), nil
+		}
+	}
+	return []byte(`"` + t.Format(format) + `"`), nil
 }
 
 func Marshal(p any) ([]byte, error) {
@@ -36,12 +52,13 @@ func Marshal(p any) ([]byte, error) {
 	newStruct := reflect.New(reflect.StructOf(newField)).Elem()
 	for i := 0; i < newStruct.NumField(); i++ {
 		oldField := ref.Field(i)
+		oldFieldType := typ.Field(i)
 		if oldField.Type().String() != "time.Time" {
 			newStruct.Field(i).Set(oldField)
 			continue
 		}
 		if v, ok := oldField.Interface().(time.Time); ok {
-			newStruct.Field(i).Set(reflect.ValueOf(DateTime(v)))
+			newStruct.Field(i).Set(reflect.ValueOf(DateTime{T: v, Tag: oldFieldType.Tag}))
 		}
 	}
 	return json.Marshal(newStruct.Interface())
