21aec420151c49d765500f8ff412ffcf73933f91
zhoujiandong <fire1220@live.cn>
2024-04-08 23:48:22 +0800

    复值上下文的key和val到新的上下文

--- common/context.go
+++ common/context.go
@@ -6,12 +6,52 @@ import (
 	"reflect"
 )
 
+// ContextDuplicate 复值上下文的key和val到新的上下文
+func ContextDuplicate(ctx context.Context) context.Context {
+	oldCtx := ctx
+	newCtx := context.Background()
+PARENT:
+	rCtx := reflect.ValueOf(ctx)
+	vCtx := rCtx
+	if rCtx.Kind() == reflect.Pointer {
+		vCtx = rCtx.Elem()
+	}
+	tCtx := vCtx.Type()
+	if vCtx.Kind() != reflect.Struct {
+		return newCtx
+	}
+	var key string
+	isParent := false
+	for i := 0; i < vCtx.NumField(); i++ {
+		v := vCtx.Field(i)
+		t := tCtx.Field(i)
+		if t.Name == "key" {
+			key = fmt.Sprintf("%v", v)
+			break
+		}
+		if t.Name == "Context" {
+			ctx, isParent = v.Interface().(context.Context)
+		}
+	}
+	if key != "" {
+		val := oldCtx.Value(key)
+		newCtx = context.WithValue(newCtx, key, val)
+	}
+	if isParent {
+		goto PARENT
+	}
+	return newCtx
+}
+
 // ContextKeys 获取上下文的所有key
 func ContextKeys(ctx context.Context) []string {
 	keys := make([]string, 0)
 PARENT:
 	rCtx := reflect.ValueOf(ctx)
-	vCtx := rCtx.Elem()
+	vCtx := rCtx
+	if rCtx.Kind() == reflect.Pointer {
+		vCtx = rCtx.Elem()
+	}
 	tCtx := vCtx.Type()
 	isParent := false
 	if vCtx.Kind() != reflect.Struct {
